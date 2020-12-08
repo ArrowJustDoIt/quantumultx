a1010c490248dd7c748fcaf0fe6eb4ff63cb5b4a
ArrowJustDoIt <arrowylq@foxmail.com>
2020-12-08 11:08:46 +0800

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -1,22 +1,27 @@
 /*************************
 亚马逊商品价格监控
 *************************/
-//var url = "https://www.amazon.cn/gp/product/B07MFZXR1B?smid=A2EDK7H33M5FFG&psc=1";
-//var price = 1642;
-const url = 'https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json';
-const bodyFilter = 'var result = JSON.parse(body); return JSON.stringify(result[167]);';
+const url = "https://www.amazon.cn/gp/product/B07MFZXR1B?smid=A2EDK7H33M5FFG&psc=1";
+const price = 1642;
+const method = "GET";
+// const headers = {"Field": "test-header-param"};
+const headers = {"Field": "test-header-param"};
+// const data = {"info": "abc"};
 
 const myRequest = {
     url: url,
-    opts:{'filter': bodyFilter}
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    // body: JSON.stringify(data) // Optional.
 };
 
 $task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
     console.log(response.body);
-    $notify("T", "S", response.body);
+    $notify("Title", "Subtitle", response.body); // Success!
     $done();
 }, reason => {
-    $done();
-    console.log(reason.error);
     // reason.error
+    $notify("Title", "Subtitle", reason.error); // Error!
+    $done();
 });
