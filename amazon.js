b642ee699e843be40e13a0514b29c5182c62c512
ArrowJustDoIt <arrowylq@foxmail.com>
2020-12-08 15:07:34 +0800

    Update amazon.js
    8

--- amazon.js
+++ amazon.js
@@ -30,7 +30,7 @@ $task.fetch(myRequest).then(response => {
     console.log(amazon.title + ":" + amazon.price);
     if(amazon.price <= price){
         $.notify(
-            `🎉🎉🎉亚马逊商品价格监控`,
+            `🎉🎉🎉亚马逊商品价格监控 ¥${amazon.price}`,
             `商品名: ${amazon.title}`,
             `当前价格: ${amazon.price}`,
             {
