41ba58f960b30b9418b48c921ecd3acc563c5bf8
ArrowJustDoIt <arrowylq@foxmail.com>
2020-12-08 15:28:53 +0800

    Update amazon.js
    1

--- amazon.js
+++ amazon.js
@@ -1,24 +1,24 @@
 /*************************
 亚马逊商品价格监控
 *************************/
+//商品链接
 const url = "https://www.amazon.cn/gp/product/B07MFZXR1B?smid=A2EDK7H33M5FFG&psc=1";
+//监控价格
 const price = 1643;
+
 const method = "GET";
-// const headers = {"Field": "test-header-param"};
 const headers =  {
     "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
 };
-// const data = {"info": "abc"};
 var parsers = {
     title: new RegExp(/id="productTitle" .+>\s+(.+)\s+<\/span>/, "i"),
     price: new RegExp(/id="priceblock_ourprice" .+>￥(.+)<\/span>/, "i"),
 };
 const myRequest = {
     url: url,
-    method: method, // Optional, default GET.
-    headers: headers, // Optional.
-    // body: JSON.stringify(data) // Optional.
+    method: method,
+    headers: headers,
 };
 
 $task.fetch(myRequest).then(response => {
