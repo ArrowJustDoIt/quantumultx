27492f534aa0030ad42656be54cc5f2ddb9bff07
ArrowJustDoIt <893091568@qq.com>
2021-09-26 21:34:57 +0800

    Update 86BusNew.js

--- 86bus/86BusNew.js
+++ 86bus/86BusNew.js
@@ -12,14 +12,30 @@ const headers =  {
 };
 //双括号里面填入日期即可,比如2021-09-27 填27 多个用逗号分隔,比如27,28,29
 const alreadyBuyDate = [27];
-var data = "sid=81623050441a472d9f7b16ce0eee9aad_50706394&id=496&timetable_start_date=2021-09-26";
-
+var data = "sid=81623050441a472d9f7b16ce0eee9aad_50706394&id=496&timetable_start_date=";
+var nowDate = getNowFormatDate()
 const myRequest = {
     url: url,
     method: method,
     headers: headers,
-    body: data
+    body: data + nowDate
 };
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate;
+    return currentdate;
+}
 
 $task.fetch(myRequest).then(response => {
     const ret = response.body;
@@ -31,7 +47,7 @@ $task.fetch(myRequest).then(response => {
                 var unSale = e.seatcount - e.buy_num;
                 var timearr=e.go_date.replace(" ",":").replace(/\:/g,"-").split("-");
 
-                if(unSale > 0 && !alreadyBuyDate.includes(timearr[timearr.length - 1].toString())){
+                if(unSale > 0 && !alreadyBuyDate.includes(Number(timearr[timearr.length - 1]))){
                     str += e.go_date + ":" + unSale;
                     data += e.go_date + ","
                 }
@@ -55,3 +71,5 @@ $task.fetch(myRequest).then(response => {
     console.log(reason.error);
     $done();
 });
+
+
