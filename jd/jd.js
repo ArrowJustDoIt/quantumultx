becd73eefa9a15f8e04b670e039e85ad286a08ab
yangliuqing <yangliuqijg@yanglqdeMacBook-Pro.local>
2022-03-04 12:51:02 +0800

    爱奇艺京东监控

--- jd/jd.js
+++ jd/jd.js
@@ -28,7 +28,7 @@ $task.fetch(myRequest).then(response => {
     const tempH = html.substr(html.indexOf("(") + 1)
     const a= tempH.substr(0, tempH.lastIndexOf(')'))
     var jstr = JSON.parse(a);
-	console.log(jstr.stockInfo.stockState);
+	console.log(jstr["stockInfo"]["stockState"]);
 	console.log(jstr)
     
     /*const amazon = {
