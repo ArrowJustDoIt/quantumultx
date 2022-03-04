86519515034f5232d19f4c3aa8adfe99af00a508
yangliuqing <yangliuqijg@yanglqdeMacBook-Pro.local>
2022-03-04 12:53:57 +0800

    爱奇艺京东监控

--- jd/jd.js
+++ jd/jd.js
@@ -2,9 +2,8 @@
 京东商品价格监控
 *************************/
 //商品链接
-// const url = "https://item.jd.com/100033551654.html";
 const url = "https://item-soa.jd.com/getWareBusiness?callback=jQuery911344&skuId=100033551654&cat=17329%2C2577%2C2589&area=19_1601_50258_62835&shopId=1000004123&venderId=1000004123&paramJson=%7B%22platform2%22%3A%22100000000001%22%2C%22specialAttrStr%22%3A%22p0ppppppppp2pppppppppppp%22%2C%22skuMarkStr%22%3A%2200%22%7D&num=1";
-// const url = "https://item.jd.com/100002718287.html";
+
 
 const method = "GET";
 const headers =  {
@@ -17,12 +16,6 @@ const myRequest = {
     headers: headers,
 };
 
-/*\\
-<span id="postNotice" style=""><small><span>无货</span><b id="postTip"></b></small></span>
-
-<div id="store-prompt" class="store-prompt"><strong>无货</strong>，此商品暂时售完</div>
-<div id="store-prompt" class="store-prompt"><strong>有货</strong></div>
-*/
 $task.fetch(myRequest).then(response => {
     const html = response.body;
     const tempH = html.substr(html.indexOf("(") + 1)
@@ -37,6 +30,13 @@ $task.fetch(myRequest).then(response => {
             }
         );
     }
+    $notify(
+            `🎉🎉🎉爱奇艺有货啦`,
+            {
+                "open-url": url,
+                "media-url": "",
+            }
+        );
     $done();
 }, reason => {
     // reason.error
