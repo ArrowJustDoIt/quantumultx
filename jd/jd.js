0dd9a3e80bd5e3287e33094890c7dea64fc31d25
yangliuqing <yangliuqijg@yanglqdeMacBook-Pro.local>
2022-03-04 10:25:10 +0800

    爱奇艺京东监控

---
+++ jd/jd.js
@@ -0,0 +1,50 @@
+/*************************
+京东商品价格监控
+*************************/
+//商品链接
+const url = "https://item.jd.com/100033551654.html";
+//监控价格
+const price = 1642;
+
+const method = "GET";
+const headers =  {
+    "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
+};
+var parsers = {
+    title: new RegExp(/id="store-prompt" class="store-prompt"><strong>(.+)<\/strong>/, "i"),
+};
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+};
+
+/*\\
+
+<div id="store-prompt" class="store-prompt"><strong>无货</strong>，此商品暂时售完</div>
+
+
+<div id="store-prompt" class="store-prompt"><strong>有货</strong></div>
+*/
+$task.fetch(myRequest).then(response => {
+    const html = response.body;
+    const amazon = {
+        title: html.match(parsers.title)[1],
+    };
+    console.log(amazon.title);
+    if(amazon.title == "有货"){
+        $notify(
+            `🎉🎉🎉爱奇艺有货啦 ¥${amazon.title}`,
+            {
+                "open-url": url,
+                "media-url": "",
+            }
+        );
+    }
+    $done();
+}, reason => {
+    // reason.error
+    console.log(reason.error);
+    $done();
+});
